4c5056c582af9842911160075ce80c1b1659cb12
Vail Dorchester <vail.dorchester@colorado.edu>
2020-09-27 00:10:43 -0600

    rewriting test file to use probatur, my C++ unit test framework

--- test/_main_test.cpp
+++ test/_main_test.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <assert.h>
 
+#include "../../probatur/include/test_class.h"
 #include "../include/vector2.h"
 #include "../include/vector3.h"
 #include "../include/vector4.h"
@@ -10,20 +11,21 @@
 #include "../include/matrix4.h"
 
 
-/*
-* Test Vector2
-*/
-void test_vector2_creation(){
-    Vector2 test = Vector2(5.0, 3.0);
-    bool result = ((test.x == 5.0) && (test.y == 3.0));
-    printf("Vector2 Creation test passed: ");
-    printf(result ? "true\n" : "false\n");
-    if(!result){
-        printf("Passed x: 5.0, passed y: 3.0\n");
-        printf("Vectorx: %f, Vectory: %f\n", test.x, test.y);
+class Vector2Test : public TestClass{
+public:
+
+    static void test_vector2_creation(){
+        Vector2 test = Vector2(5.0, 3.0);
+        Vector2 test2 = Vector2(1.0, -2.0);
+        Vector2 test3 = Vector2();
+        AssertEquals(test.x, 5.0);
+        AssertEquals(test.y, 3.0);
+        AssertEquals(test2.x, 1.0);
+        AssertEquals(test2.y, -2.0);
+        AssertEquals(test3.x, 0.0);
+        AssertEquals(test3.y, 0.0);
     }
-    printf("\n");
-}
+};
 
 /*
 * Test Vector3
@@ -77,7 +79,7 @@ void test_vector3_add(){
 * Main
 */
 int main(){
-    test_vector2_creation();
+    Vector2Test::test_vector2_creation();
     test_vector3_creation();
     test_vector3_subtract();
     test_vector3_add();
